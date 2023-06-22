5d98594b194c38d30a9b756c84d4b42963fc3297
Whitley Beers <whitleybeers@gmail.com>
2023-06-21 20:46:51 -0500

    removed testing stuff

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,9 @@
 import { Button } from 'react-bootstrap';
-import { useEffect, useState } from 'react';
 import { signOut } from '../utils/auth';
 import { useAuth } from '../utils/context/authContext';
-import { getSubscriptions } from '../api/subscriptionData';
 
 function Home() {
   const { user } = useAuth();
-
-  const [subscriptions, setSubscriptions] = useState([]);
-
-  useEffect(() => {
-    getSubscriptions(user.id).then(setSubscriptions);
-  }, [user.id]);
 
   return (
     <div
@@ -25,11 +17,6 @@ function Home() {
     >
       <h1>Hello {user.fbUser.displayName}! </h1>
       <p>Your Bio: {user.bio}</p>
-      <div>Subscribed to:
-        {subscriptions.map((subscription) => (
-          <p>{subscription.author_id.first_name} {subscription.author_id.last_name}</p>
-        ))}
-      </div>
       <p>Click the button below to logout!</p>
       <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
         Sign Out

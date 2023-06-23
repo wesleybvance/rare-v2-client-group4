109722137a828ccd608ee694465561cc577b8774
Wesley Vance <wslyvnce@gmail.com>
2023-06-23 11:56:59 -0500

    create comment, styling comment card, ternary for edit/delete buttons

--- pages/posts/post.js
+++ pages/posts/post.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useRouter } from 'next/router';
